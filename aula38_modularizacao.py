18ae440eaf7e808752ba3cf7ea640dfcea656c92
Victor Rafael <victorafael01@hotmail.com>
2025-11-12 11:00:32 -0300

    Adicao da aula 38

---
+++ aula38_modularizacao.py
@@ -0,0 +1,13 @@
+# Entendendo os seus próprios módulos Python
+# O primeiro módulo executado chama-se __main__
+# Você pode importar outro módulo inteiro ou parte do módulo
+# o Python conhece a pasta onde o __main__ está e as pastas abaixo dele
+# Ele não reconhece pastas e módulos acima do __main__ por padrão
+# O Python conhece todos os módulos e pacotes presentes nos caminhos de sys.path
+import sys
+sys.path
+
+import aula15_set
+
+print('Este módulo se chama', __name__)
+print(*sys.path, sep='\n')
